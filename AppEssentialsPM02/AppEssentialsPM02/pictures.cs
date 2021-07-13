9364db529c01d2b333ef6cfe79f48dca553ea7e2
dmhernanandez <dany1999hernan@gmail.com>
2021-07-13 17:53:52 -0600

    Agregar archivos de proyecto.

---
+++ AppEssentialsPM02/AppEssentialsPM02/pictures.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppEssentialsPM02
+{
+
+    public class pictures
+    {
+        [AutoIncrement, PrimaryKey]
+        public int id { set; get; }
+
+        public string ImageRoute { get; set; }
+
+        //public string Nombre { get; set; }
+    }
+}
